09728025e3ef79fef7e5fb18ce94ace1cc09f0b7
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-10-01 11:05:04 +0530

    made price and repo

--- pkg/shop/application/products.go
+++ pkg/shop/application/products.go
@@ -17,6 +17,18 @@ func (s ProductsService) AllProducts() {
 
 }
 
-func (s ProductsService) AddProduct() error {
+type AddProductCommand struct {
+	ID            string
+	Name          string
+	Description   string
+	ProceCents    uint
+	PriceCurrency uint
+}
 
+func (s ProductsService) AddProduct(cmd AddProductCommand) error {
+	price.NewPrice(cmd.ProceCents, cmd.PriceCurrency)
+
+	products.NewProduct(products.ID(cmd.ID), cmd.Name, cmd.Description, price)
+
+	s.repo.Save
 }
