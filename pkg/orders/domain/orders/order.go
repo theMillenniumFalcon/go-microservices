de7fa3a82303a92b52432086be269b0585da02dd
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-10-02 15:42:18 +0530

    order and product added

---
+++ pkg/orders/domain/orders/order.go
@@ -0,0 +1,38 @@
+package orders
+
+import "errors"
+
+type ID string
+
+var ErrEmptyOrderID = errors.New("empty order id")
+
+type Order struct {
+	id      ID
+	product Product
+	address Address
+	paid    bool
+}
+
+func (o *Order) ID() ID {
+	return o.id
+}
+
+func (o Order) Product() Product {
+	return o.product
+}
+
+func (o Order) Address() Address {
+	return o.address
+}
+
+func (o *Order) MarkAsPaid() {
+	o.paid = true
+}
+
+func NewOrder(id ID, product Product, address Address) (*Order, error) {
+	if len(id) == 0 {
+		return nil, ErrEmptyOrderID
+	}
+
+	return &Order{id, product, address, false}, nil
+}
---
+++ pkg/orders/domain/orders/product.go
@@ -0,0 +1,33 @@
+package orders
+
+import "errors"
+
+type ProductID string
+
+var ErrEmptyProductID = errors.New("empty product ID")
+
+type Product struct {
+	id    ProductID
+	name  string
+	price price.Price
+}
+
+func (p Product) ID() ProductID {
+	return p.id
+}
+
+func (p Product) Name() string {
+	return p.name
+}
+
+func (p Product) Price() price.Price {
+	return p.price
+}
+
+func NewProduct(id ProductID, name string, price price.Price) (Product, error) {
+	if len(id) == 0 {
+		return Product{}, ErrEmptyProductID
+	}
+
+	return Product{id, name, price}, nil
+}
