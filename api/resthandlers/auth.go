0127ce227ee290460e8434cb920faa14e08ff924
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-10-16 15:06:41 +0530

    added auth methods

--- api/resthandlers/auth.go
+++ api/resthandlers/auth.go
@@ -2,10 +2,12 @@ package resthandlers
 
 import (
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/theMillenniumFalcon/microservices/api/restutil"
 	"github.com/theMillenniumFalcon/microservices/pb"
 	"gopkg.in/mgo.v2/bson"
@@ -57,4 +59,72 @@ func (h *authHandlers) SignUp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	restutil.WriteAsJson(w, http.StatusCreated, resp)
+}
+
+func (h *authHandlers) PutUser(w http.ResponseWriter, r *http.Request) {
+	if r.Body == nil {
+		restutil.WriteError(w, http.StatusBadRequest, restutil.ErrEmptyBody)
+		return
+	}
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		restutil.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+	user := new(pb.User)
+	err = json.Unmarshal(body, user)
+	if err != nil {
+		restutil.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+	vars := mux.Vars{r}
+	user.Id = vars["id"]
+	resp, err := h.authSvcClient.UpdateUser(r.Context(), user)
+	if err != nil {
+		restutil.WriteError(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	restutil.WriteAsJson(w, http.StatusOK, resp)
+}
+
+func (h *authHandlers) GetUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars{r}
+	resp, err := h.authSvcClient.GetUser(r.Context(), &pb.GetUserRequest{Id: vars["id"]})
+	if err != nil {
+		restutil.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+	restutil.WriteAsJson(w, http.StatusOK, resp)
+}
+
+func (h *authHandlers) GetUsers(w http.ResponseWriter, r *http.Request) {
+	stream, err := h.authSvcClient.ListUsers(r.Context(), &pb.ListUsersRequest{})
+	if err != nil {
+		restutil.WriteError(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	var users []*pb.User
+	for {
+		user, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			restutil.WriteError(w, http.StatusBadRequest, err)
+			return
+		}
+		users = append(users, user)
+	}
+	restutil.WriteAsJson(w, http.StatusOK, users)
+}
+
+func (h *authHandlers) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	resp, err := h.authSvcClient.DeleteUser(r.Context(), &pb.GetUserRequest{Id: tokenPayload.UserId})
+	if err != nil {
+		restutil.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+	w.Header().Set("Entity", resp.Id)
+	restutil.WriteAsJson(w, http.StatusNoContent, nil)
 }
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/mux v1.8.0
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
 	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
