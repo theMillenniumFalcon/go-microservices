34faf2b074ea57f7509c19b30407a6a662d9dbd0
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-10-22 22:45:29 +0530

    jwt tokens and tests added

--- security/tokens.go
+++ security/tokens.go
@@ -18,6 +18,12 @@ var (
 	jwtSecretKey = []byte(os.Getenv("JWT_SECRET"))
 )
 
+type TokenPayload struct {
+	UserId    string
+	CreatedAt time.Time
+	ExpiresAt time.Time
+}
+
 func NewToken(userId string) (string, error) {
 	claims := &jwt.StandardClaims{
 		ExpiresAt: time.Now().Add(time.Minute * 5).Unix(),
@@ -44,4 +50,27 @@ func ExtractToken(r *http.Request) (string, error) {
 		return "", ErrInvalidToken
 	}
 	return splitted[1], nil
+}
+
+func ParseToken(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, parseJwtCallback)
+}
+
+func NewTokenPayload(tokenString string) (*TokenPayload, error) {
+	token, err := ParseToken(tokenString)
+	if err != nil {
+		return nil, err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !token.Valid || !ok {
+		return nil, ErrInvalidToken
+	}
+	id, _ := claims["iss"].(string)
+	createdAt, _ := claims["iat"].(float64)
+	expiresAt, _ := claims["exp"].(float64)
+	return &TokenPayload{
+		UserId:    id,
+		CreatedAt: time.Unix(int64(createdAt), 0),
+		ExpiresAt: time.Unix(int64(expiresAt), 0),
+	}, nil
 }
